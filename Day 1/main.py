450c28e547c5378d5045d749e91e70e944cbfcb0
kj <kashishj197@gmail.com>
2022-06-13 10:36:30 +0530

    Day 1

---
+++ Day 1/main.py
@@ -0,0 +1,18 @@
+# # Lesson 1: How to print in console # #
+print("Hello World")
+
+# # Lesson 2: Python print function # #
+print("print(\"what to print\")")
+# OR
+# print("print('what to print')")
+
+# # Lesson 3: String manipulation and code intelligence # #
+print("Hello" + " " + "Kashish")
+# Errors: SyntaxError, IndentationError
+
+# # Lesson 4: Input function # #
+print("Hello " + input("What is your name? ") + "!")
+
+# # Lesson 5: Input exercise # #
+name = input("What is your name? ")
+print(len(name))
